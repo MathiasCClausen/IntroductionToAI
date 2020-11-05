239c30de018b133147c4a5781f22aaad2e37d1d5
mathiaskloch57 <maklo16@student.sdu.dk>
2020-11-05 09:08:20 +0100

    update

--- drive.py
+++ drive.py
@@ -3,10 +3,14 @@ from ev3dev.ev3 import *
 from time import sleep
 
 
+
+
 # Initialize the motors.
 # Highest value 900 and lowest value -900
 mLeft = LargeMotor('outB')
 mRight = LargeMotor('outC')
+
+#robot = DriveBase(mLeft, mRight, wheel_diameter=55.5, axle_track=120)
 
 # Put the color sensor into COL-REFLECT mode
 # to measure reflected light intensity.
@@ -25,112 +29,136 @@ clLeft.mode='COL-REFLECT'
 def FollowLine():
 
     while True:
-        mLeft.run_forever(speed_sp=350)
-        mRight.run_forever(speed_sp=350)
+        mLeft.run_forever(speed_sp=450)
+        mRight.run_forever(speed_sp=450)
 
         if clLeft.value() < 40:
-            mLeft.run_forever(speed_sp=0)
+            mLeft.run_forever(speed_sp=000)
             mRight.run_forever(speed_sp=250)
 
         if clRight.value() < 40:
             mLeft.run_forever(speed_sp=250)
-            mRight.run_forever(speed_sp=0) 
+            mRight.run_forever(speed_sp=000) 
     
         if clLeft.value() < 40 and clRight.value() < 40:
             break 
 
+def PushCan():
+    while True:
+        mLeft.run_forever(speed_sp=350)
+        mRight.run_forever(speed_sp=350)
+
+        if clLeft.value() < 40:
+            mLeft.run_forever(speed_sp=00)
+            mRight.run_forever(speed_sp=250)
+
+        if clRight.value() < 40:
+            mLeft.run_forever(speed_sp=250)
+            mRight.run_forever(speed_sp=00) 
+    
+        if clLeft.value() < 40 and clRight.value() < 40:
+            break
+
+
 def BackWard():
     while True:
-        mLeft.run_forever(speed_sp=-250)
-        mRight.run_forever(speed_sp=-250)
+        mLeft.run_forever(speed_sp=-350)
+        mRight.run_forever(speed_sp=-350)
         
         if clLeft.value() < 40 and clRight.value() < 40:
             break
 
 def TurnRight():
     while True:
-        mLeft.run_forever(speed_sp=350)
-        mRight.run_forever(speed_sp=-100)
-
-        if clLeft.value() < 40 and clRight.value() < 40:
-            mLeft.run_forever(speed_sp=350)
-            mRight.run_forever(speed_sp=-100)
-        if clLeft.value() > 40 and clRight.value() < 40:
-            mLeft.run_forever(speed_sp=350)
-            mRight.run_forever(speed_sp=-100)
+    
+        mLeft.run_to_rel_pos(position_sp=180, speed_sp=400, stop_action="hold")
+        mRight.run_to_rel_pos(position_sp=-180, speed_sp=250, stop_action="hold")
+        sleep(1)
         
-        if clLeft.value() > 40 and clRight.value() > 40:
-            break
+        break
+        
+        
 
 def TurnLeft():
+    
     while True:
-        mLeft.run_forever(speed_sp=-100)
-        mRight.run_forever(speed_sp=350)
-
-        if clLeft.value() < 40 and clRight.value() < 40:
-            mLeft.run_forever(speed_sp=-100)
-            mRight.run_forever(speed_sp=350)
-
-        if clLeft.value() < 40 and clRight.value() > 40:
-            mLeft.run_forever(speed_sp=-100)
-            mRight.run_forever(speed_sp=350)
-
-        if clLeft.value() > 40 and clRight.value() > 40:
-            break
+        mLeft.run_to_rel_pos(position_sp=-180, speed_sp=250, stop_action="hold")
+        mRight.run_to_rel_pos(position_sp=180, speed_sp=400, stop_action="hold")
+        sleep(1)
         
+        break
+       
+
+def TurnAround():
+    while True:
+        mLeft.run_to_rel_pos(position_sp=380, speed_sp=350, stop_action="hold")
+        mRight.run_to_rel_pos(position_sp=-380, speed_sp=350, stop_action="hold")
+        sleep(1)
+        
+        break
+    
+        
+
         
 def GoOverFoward(): 
     while True: 
-        mLeft.run_forever(speed_sp=250)
-        mRight.run_forever(speed_sp=250)
+        mLeft.run_forever(speed_sp=350)
+        mRight.run_forever(speed_sp=350)
 
         if clLeft.value() < 40 and clRight.value() < 40:
-            mLeft.run_forever(speed_sp=250)
-            mRight.run_forever(speed_sp=250)
+            mLeft.run_forever(speed_sp=350)
+            mRight.run_forever(speed_sp=350)
 
         if clLeft.value() > 40 and clRight.value() > 40:
             break
 
 def GoOverBackward(): 
     while True: 
-        mLeft.run_forever(speed_sp=-250)
-        mRight.run_forever(speed_sp=-250)
+        mLeft.run_forever(speed_sp=-350)
+        mRight.run_forever(speed_sp=-350)
 
         if clLeft.value() < 40 and clRight.value() < 40:
-            mLeft.run_forever(speed_sp=-250)
-            mRight.run_forever(speed_sp=-250)
+            mLeft.run_forever(speed_sp=-350)
+            mRight.run_forever(speed_sp=-350)
 
         if clLeft.value() > 40 and clRight.value() > 40:
             break
 
 
-Commands = ['f','f','f','f','l','f','f','f','b','b','b']
 
-def move(commands)
-    for i in range(commands):
+Commands = ['f','f','pf','b','r','f','l','f','l','f','f','p','b','a']
+
+def move(commands):
+    
+    for i in range(len(commands)):
         if(commands[i] == 'f'):
-            if commands[i+1] == 'f':
-                FollowLine()
-                GoOverFoward()
+            FollowLine() 
+            GoOverFoward()
                 
-            else   
-                FollowLine()
+        if(commands[i] == 'pf'):   
+            PushCan()
+                
                 
 
         if(commands[i] == 'b'):
-            if(commands[i+1] == 'b'):
-                BackWard()
-                GoOverBackward()
+            mLeft.run_forever(speed_sp=0)
+            mRight.run_forever(speed_sp=0)
+            
+            GoOverBackward()
+            sleep(1)
+            BackWard()
                 
-            else
-                BackWard()
                 
         if(commands[i] == 'r'):
             TurnRight()
             
         if(commands[i] == 'l'):
             TurnLeft()
+
+        if(commands[i] == 'a'):
+            TurnAround()
              
+
 
 move(Commands)
 
